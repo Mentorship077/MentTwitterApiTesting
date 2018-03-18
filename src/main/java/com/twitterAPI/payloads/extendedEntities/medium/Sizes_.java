3d5aeb5a44802c91444550c8a8bff3b50aa6cf7b
Nazar <khimin1719@gmail.com>
2018-03-18 13:48:27 +0200

    was added payload of twitter post

---
+++ src/main/java/com/twitterAPI/payloads/extendedEntities/medium/Sizes_.java
@@ -0,0 +1,87 @@
+
+package com.twitterAPI.payloads.extendedEntities.medium;
+
+import java.util.HashMap;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.twitterAPI.payloads.extendedEntities.medium.sizes.Large_;
+import com.twitterAPI.payloads.extendedEntities.medium.sizes.Medium___;
+import com.twitterAPI.payloads.extendedEntities.medium.sizes.Small_;
+import com.twitterAPI.payloads.extendedEntities.medium.sizes.Thumb_;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "thumb",
+    "small",
+    "medium",
+    "large"
+})
+public class Sizes_ {
+
+    @JsonProperty("thumb")
+    private Thumb_ thumb;
+    @JsonProperty("small")
+    private Small_ small;
+    @JsonProperty("medium")
+    private Medium___ medium;
+    @JsonProperty("large")
+    private Large_ large;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("thumb")
+    public Thumb_ getThumb() {
+        return thumb;
+    }
+
+    @JsonProperty("thumb")
+    public void setThumb(Thumb_ thumb) {
+        this.thumb = thumb;
+    }
+
+    @JsonProperty("small")
+    public Small_ getSmall() {
+        return small;
+    }
+
+    @JsonProperty("small")
+    public void setSmall(Small_ small) {
+        this.small = small;
+    }
+
+    @JsonProperty("medium")
+    public Medium___ getMedium() {
+        return medium;
+    }
+
+    @JsonProperty("medium")
+    public void setMedium(Medium___ medium) {
+        this.medium = medium;
+    }
+
+    @JsonProperty("large")
+    public Large_ getLarge() {
+        return large;
+    }
+
+    @JsonProperty("large")
+    public void setLarge(Large_ large) {
+        this.large = large;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
--- src/test/java/com/twitterAPI/HelloWorldRestAssured.java
+++ src/test/java/com/twitterAPI/HelloWorldRestAssured.java
@@ -1,6 +1,12 @@
 package com.twitterAPI;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.twitterAPI.api.TweetsApi;
+import com.twitterAPI.payloads.HomeTimeLine;
 import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
 
@@ -8,5 +14,16 @@ public class HelloWorldRestAssured {
     @Test
     public void makeSureThatGoogleIsUp() {
         given().when().get("http://www.google.com").then().statusCode(200);
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        TweetsApi tweetsApi = new TweetsApi();
+
+//        System.out.println(tweetsApi.homeTimeLine().asString());
+        ObjectMapper mapper = new ObjectMapper();
+        HomeTimeLine[] api = mapper.readValue(tweetsApi.homeTimeLine().asString(), HomeTimeLine[].class);
+        System.out.println(api[1].toString());
+
     }
 }
