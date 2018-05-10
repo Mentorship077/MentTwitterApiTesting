5f0eaf2327e13bbf567f90bc0183e554bfb6d563
Nazar_Khimin <Nazar_Khimin@epam.com>
2018-05-10 17:59:29 +0300

    merge

--- src/test/java/com/twitterAPI/BaseTestClass.java
+++ src/test/java/com/twitterAPI/BaseTestClass.java
@@ -2,4 +2,18 @@ package com.twitterAPI;
 
 public abstract class BaseTestClass {
 
+//   @Test
+//    public void shouldReturnPersonForTheId() {
+//        given().
+//                accept(ContentType.JSON).
+//                pathParam("id", 1).
+//                when().
+//                get("/people/{id}").
+//                then().
+//                statusCode(200).
+//                body(
+//                        "id", is(1),
+//                        "name", is("Praveer")
+//                );
+//    }
 }
